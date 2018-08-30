715336a750a2a1eb2a37bbcdd6b68a711ed32e30
OscarOteiza <oscaroteiza@intelligens.ai>
2018-08-30 15:43:22 -0300

    Update setvars.sh
    added account, auth and workspace IDs

--- setvars.sh
+++ setvars.sh
@@ -1,8 +1,8 @@
 echo "+++ Set variables."
 # ------------------------------------------------------------------------------
-ACCOUNT_SID=your_account_SID
-AUTH_TOKEN=your_account_auth_token
-WORKSPACE_SID=your_TaskRouter_workspace_SID
+ACCOUNT_SID=AC8802ce81301592cba8b9e12dcf98c737
+AUTH_TOKEN=99ac039752088601314d6b1d3a56a64d
+WORKSPACE_SID=WS05915854bbc946f3593cc7e4799a39a5
 export ACCOUNT_SID
 export AUTH_TOKEN
 export WORKSPACE_SID
