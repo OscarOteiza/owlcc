abea2387f23999df39677c6140e6f4a7d9e45ddf
OscarOteiza <oscaroteiza@intelligens.ai>
2018-09-11 12:40:18 -0300

    added data to try to record calls

--- custom/index.php
+++ custom/index.php
@@ -1,1 +1,14 @@
-hola
+<?php
+// Get the PHP helper library from https://twilio.com/docs/libraries/php
+require __DIR__ . '/twilio-php-master/Twilio/autoload.php';
+
+# Start our TwiML response
+$response = new Twilio\TwiML();
+
+# Use <Record> to record the caller's message
+$response->record();
+
+# End the call with <Hangup>
+$response->hangup();
+
+echo $response;
